7c7d72edd15bedf2598d0a4509107344797c59ff
brunocalou <brunocaloualves@gmail.com>
2016-11-19 12:31:17 -0200

    Added easter egg

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
-  <div id="app">
-    <home></home>
-  </div>
+	<div id="app">
+		<home></home>
+		<easter-egg></easter-egg>
+	</div>
 </template>
 
 <script>
 import Home from './components/Home.vue'
+import EasterEgg from './components/EasterEgg/EasterEgg.vue'
 
 export default {
-  name: 'app',
-  components: {
-    Home
-  }
+	name: 'app',
+	components: {
+		Home, EasterEgg
+	}
 }
 </script>
 
